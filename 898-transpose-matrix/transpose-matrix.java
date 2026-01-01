898adc7dc617117fb52afeabd04f5a6109f8bbe9
suhanasinha026-sudo <suhanasinha026@gmail.com>
2026-01-01 23:54:39 +0530

    Time: 1 ms (26.03%) | Memory: 46.7 MB (44.96%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int temp;
+        int t[][] = new int[matrix[0].length][matrix.length];
+
+        for(int i=0; i<matrix.length; i++)
+        {
+            for(int j=0; j<matrix[0].length; j++)
+            {
+                t[j][i]=matrix[i][j];
+                
+            }
+        }
+
+        return t;
+        
+    }
+}
