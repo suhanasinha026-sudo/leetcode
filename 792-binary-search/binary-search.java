f96356b0d9977bdad830aafd1d280dc5dbec3815
suhanasinha026-sudo <suhanasinha026@gmail.com>
2026-01-15 23:59:11 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (91.75%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+
+        while(start<=end)
+        {
+            int middle=start+((end - start)/2);
+            if(nums[middle]>target)
+            {
+                end=middle-1;
+            }
+            else if(nums[middle]<target)
+            {
+                start=middle+1;
+            }
+            else
+                return middle;
+        }
+        return -1;
+        
+    }
+}
